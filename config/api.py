66b6e7834fa435a03df21b54c18a3f856c34b455
= <=>
2022-12-23 12:22:35 +0000

    get

--- config/api.py
+++ config/api.py
@@ -13,3 +13,13 @@ def add(request, a:int, b:int):
 @api.get("/hello")
 def hello(request, name:str="world"):
     return f"Hello {name}"
+
+@api.get("/math")
+def math(request, a:int, b:int):
+    return {"add": a+b, "multiply": a*b}
+
+@api.get("/math{a}and{b}")
+def math2(request, a:int, b:int):
+    return {"add":a+b, "multiply":a*b}
+
+
